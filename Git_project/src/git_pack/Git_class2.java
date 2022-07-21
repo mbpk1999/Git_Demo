cf440925a70513dfa1c1c1ecbdb495575cdeae20
Bala Pradeep Kumar M <87418278+mbpk1999@users.noreply.github.com>
2022-07-21 23:42:17 +0530

    Update Git_class2.java
    Github changes Git_class2

--- Git_project/src/git_pack/Git_class2.java
+++ Git_project/src/git_pack/Git_class2.java
@@ -4,6 +4,7 @@ public class Git_class2 {
 
 	public static void main(String[] args) {
 		System.out.println("Created a new class called 'Git_Class2'");
+		System.out.println("Editing from Github");
 	}
 
 }
