72b957e567308cd443f4f1423b0d602db53b3ed2
balap <balap@MBPK>
2023-01-08 21:57:00 +0530

    New changes on 08/01/2023

--- Git_project/src/git_pack/Git_class.java
+++ Git_project/src/git_pack/Git_class.java
@@ -4,6 +4,7 @@ public class Git_class {
 
 	public static void main(String[] args) {
 		System.out.println("This is a sample program to work with GIThub");
+		System.out.println("After long time....");
 	}
 
 }
